09c13c6963a8d7b142d01ee67e4c14434c37ec33
Rob Merrill <robgmerrill@gmail.com>
2018-11-10 09:57:53 -0800

    Style components

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import gatsbyLogo from '../images/gatsby-icon.png'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  img {
+    margin-bottom: 0
+  }
+`;
+
+const HeaderContainer  = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1rem 1rem;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -34,8 +38,8 @@ const Header = ({ siteTitle }) => (
           />
         </Link>
       </h1>
-    </div>
-  </div>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Header from './header'
 import Archive from './archive'
 import './layout.css'
+
+const MainLayout = styled.main`
+  max-width: 90%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -31,18 +39,13 @@ const Layout = ({ children }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-        <Archive />
-      </>
+        <MainLayout>
+          <div>
+            {children}
+          </div>
+          <Archive />
+        </MainLayout>
+        </>
     )}
   />
 )
